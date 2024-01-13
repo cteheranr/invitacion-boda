ba6d43b51960de61630c1f831bd6395b60673cbd
Camilo Andres Teheran Rodriguez <kmilo.teheran@gmail.com>
2024-01-13 16:36:56 -0500

    Se agregan emojis al mensaje de whp

--- src/components/confirma/Confirma.jsx
+++ src/components/confirma/Confirma.jsx
@@ -4,7 +4,7 @@ import './confirma.css'
 function Confirma() {
     return (
         <div className='confirma'>
-            <a target="_blank" href="https://wa.me/573052929686?text=%F0%9F%91%8D%20Hola%20Camilo%20y%20Andrea,%0D%0AQuiero%20confirmar%20mi%20asistencia%20a%20su%20boda.">
+            <a target="_blank" href="https://wa.me/573052929686?text=Hola%20Camilo%20y%20Andrea,%20❤️%0D%0AQuiero%20confirmar%20mi%20asistencia%20a%20su%20boda.%20🤵👰🎉🎊">
                 <button>Confirma tu asistencia</button>
             </a>
         </div >
