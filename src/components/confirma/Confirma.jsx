8124e6d544cbfb8021883e29a460981a1efe7d55
Camilo Andres Teheran Rodriguez <kmilo.teheran@gmail.com>
2024-01-13 16:19:19 -0500

    Se arreglan unos estilos

--- src/components/confirma/Confirma.jsx
+++ src/components/confirma/Confirma.jsx
@@ -7,7 +7,6 @@ function Confirma() {
             <a target="_blank" href="https://wa.me/573052929686?text=%F0%9F%91%8D%20Hola%20Camilo%20y%20Andrea,%0D%0AQuiero%20confirmar%20mi%20asistencia%20a%20su%20boda.">
                 <button>Confirma tu asistencia</button>
             </a>
-            <button>Descarga tu invitación</button>
         </div >
     )
 }
