65136d674f7b46f073444798c10949e70c7eda77
Camilo Andres Teheran Rodriguez <kmilo.teheran@gmail.com>
2024-05-07 21:51:18 -0500

    Se agregan los ultimos cambios

--- src/components/galeria/Galeria.jsx
+++ src/components/galeria/Galeria.jsx
@@ -1,23 +1,37 @@
 import React from 'react'
-import foto1 from '../../assets/foto1.jpeg'
-import foto2 from '../../assets/foto2.jpeg'
-import foto3 from '../../assets/foto3.jpeg'
-import foto4 from '../../assets/foto4.jpeg'
-import foto5 from '../../assets/foto5.jpeg'
+import foto1 from '../../assets/foto1.jpg'
+import foto2 from '../../assets/foto2.jpg'
+import foto3 from '../../assets/foto3.jpg'
+import foto4 from '../../assets/foto4.jpg'
+import foto5 from '../../assets/foto5.jpg'
+import foto6 from '../../assets/foto6.jpg'
 import './galeria.css'
 
 function Galeria() {
     return (
         <>
             <div className='content-column'>
-            <h3 className='title-church'>Galeria</h3>
-            <div className='gallery'>
-                    <img src={foto1} alt="" />
-                    <img src={foto2} alt="" />
-                    <img src={foto3} alt="" />
-                    <img src={foto4} alt="" />
-                    <img src={foto5} alt="" />
+                <h3 className='title-church'>Galeria</h3>
+                <div className='gallery'>
+                    <div className="img1">
+                        <img src={foto1} alt="" />
                     </div>
+                    <div className="img2">
+                        <img src={foto2} alt="" />
+                    </div>
+                    <div className="img3">
+                        <img src={foto3} alt="" />
+                    </div>
+                    <div className="img4">
+                        <img src={foto4} alt="" />
+                    </div>
+                    <div className="img5">
+                        <img src={foto5} alt="" />
+                    </div>
+                    <div className="img6">
+                        <img src={foto6} alt="" />
+                    </div>
+                </div>
             </div>
         </>
     )
