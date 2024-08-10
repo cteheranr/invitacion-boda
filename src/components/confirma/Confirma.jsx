c9233896a2d1be2a3f6acf3c1ae88c2f55857527
cteheranr <kmilo.teheran@gmail.com>
2024-08-09 20:43:09 -0500

    Se agrega boton

--- src/components/confirma/Confirma.jsx
+++ src/components/confirma/Confirma.jsx
@@ -5,7 +5,10 @@ function Confirma() {
     return (
         <div className='confirma'>
             <a target="_blank" href="https://wa.me/573052929686?text=Hola%20Camilo%20y%20Andrea,%20❤️%0D%0AQuiero%20confirmar%20mi%20asistencia%20a%20su%20boda.%20🤵👰🎉🎊">
-                <button>Confirma tu asistencia</button>
+                <button>Confirma tu asistencia a Andrea Meza</button>
+            </a>
+            <a target="_blank" href="https://wa.me/573022101380?text=Hola%20Camilo%20y%20Andrea,%20❤️%0D%0AQuiero%20confirmar%20mi%20asistencia%20a%20su%20boda.%20🤵👰🎉🎊">
+                <button>Confirma tu asistencia a Camilo Teherán</button>
             </a>
         </div >
     )
